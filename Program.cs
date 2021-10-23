998da93c04045541418590a4805975692ac86005
DavidLucas2021 <dlaredes@gmail.com>
2021-10-23 09:50:57 -0300

    Membros Protected

--- Program.cs
+++ Program.cs
@@ -4,69 +4,43 @@ public class Principal
 {
     static void Main()
     {
-        Carro c1 = new Carro("sim");
-        Console.WriteLine("Portas fechadas: " + c1.portasfechada);
-        Console.WriteLine("Chave: " + c1.GetChave());
-        Console.WriteLine("Ligado: " + c1.ligar());
-        Console.WriteLine("Velocidade: " + c1.velocidade);
-
-        // CarroGrande cg1 = new CarroGrande();
-        // Console.WriteLine("Portas: " + cg1.portas);
-        // Console.WriteLine("Portas fechadas: " + cg1.portasfechada);
-        // Console.WriteLine("Chave: " + cg1.GetChave());
-        // Console.WriteLine("Ligado: " + cg1.ligar());
-        // Console.WriteLine("Velocidade: " + cg1.velocidade);
-
+        Carro Uno = new Carro("Uno Way",130);
+        Console.WriteLine("Nome:................"+Uno.nome);
+        Console.WriteLine("Esta ligado:........."+Uno.GetLigado());
+        Console.WriteLine("Velocidade atual:...."+Uno.Vel_Atual);
+        Console.WriteLine("Velocidade Máxima:..."+Uno.GetVel_Max());
     }
 }
 public class Veiculo //Classe Base
 {
-    public int velocidade;
-    public bool ligado;
-    private string chave;
+    public int Vel_Atual;
+    private int Vel_Max;
+    protected bool ligado;
 
-    public string portasfechada;
+    public Veiculo(int Vel_Max)
+    {
+        Vel_Atual=0;
+        this.Vel_Max=Vel_Max;
+        ligado=false;  
+    }
+    public bool GetLigado()
+    {
+        return ligado;
+    }
+    public int GetVel_Max()
+    {
+        return Vel_Max;
+    }
 
-    public string ligar()
-    {
-        if (velocidade > 0 && GetChave() == "sim")
-        {
-            ligado = true;
-            return "sim";
-        }
-        else
-        {
-            ligado = false;
-            return "não";
-        }
-    }
-    public Veiculo()
-    {
-        string chave;
-        chave = Console.ReadLine();
-        Console.Clear();
-        this.chave = chave;
-    }
-    public string GetChave()
-    {
-        return chave;
-    }
 }
-public class Carro : Veiculo  //Classe Carro = Classe Derivada(filha) 
+public class Carro : Veiculo //Classe derivada
 {
-    public Carro(string fechado) : base()
+    public string nome;
+    public Carro(string nome, int Vel_M):base(Vel_M) 
     {
-        portasfechada = fechado;
-        velocidade = int.Parse(Console.ReadLine());
-        Console.Clear();
-    }
-}
-public class CarroGrande : Carro
-{
-    public int portas;
-    public CarroGrande() : base("sim")
-    { 
-        portas=6;
+        this.nome=nome;
+        ligado=true;
+        Vel_Atual=30;
     }
 }
 
