733031eaae2f358aa0a7a3a358c05294e9d7b3be
DavidLucas2021 <dlaredes@gmail.com>
2021-10-24 17:47:19 -0300

    Métodos virtuais

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ public class Principal
     static void Main()
     {
         Derivada2 d2 = new Derivada2();
+        d2.informacao();
     }
 }
 public class Base
@@ -13,14 +14,21 @@ public class Base
     {
         Console.WriteLine("Construtor Base");
     }
+    virtual public void informacao(){}
 }
+
 public class Derivada1:Base
 {
     public Derivada1()
     {
         Console.WriteLine("Construtor da classe Derivada 1");
     }
+    override public void informacao()
+    {
+        Console.WriteLine("Método virtual da classe Base na classe Drivada 1");
+    }
 }
+
 public class Derivada2:Derivada1
 {
     public Derivada2()
