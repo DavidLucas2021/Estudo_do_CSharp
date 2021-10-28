7815060b41de445fb86514a08b5b4ec88fe5deef
DavidLucas2021 <dlaredes@gmail.com>
2021-10-28 20:00:58 -0300

    Acessors Get e Set

--- Program.cs
+++ Program.cs
@@ -3,53 +3,36 @@
 public class Principal
 {
     static void Main()
-    {
-        Carro Argo  = new Carro();
-        Argo.aceleração(5);
-        Console.WriteLine("ligado;.............."+Argo.GetLigado(true));
-        Console.WriteLine("Velocidade Atual:...."+Argo.GetVel_Atual());
-        Console.WriteLine("Velocidade Atual:...."+Argo.GetVel_Max());
-        
-        
+    {   
+        Carro Uno = new Carro();
+        Uno.vm=340;
+        Console.WriteLine("Velocidade Máxima:...."+Uno.vm);        
     }
 }
-abstract class Veiculo
+class Carro
 {
-    protected int Vel_Max;
-    protected int Vel_Atual;
-    protected bool Ligado;
-
-    public Veiculo()
-    {
-        Ligado=false;
-        Vel_Atual=0;
-    }
-    public string GetLigado(bool ligado)
-    {
-        this.Ligado=ligado;
-        if(ligado == true)
-        {
-            return "sim";
-        }else return "não";
-    }
-    abstract public void aceleração(int aum);
-}
-class Carro:Veiculo
-{
+    private int Vel_Max;
     public Carro()
     {
-        Vel_Max=110;
+        vm=120;
     }
-    override public void aceleração(int aumentar)
-    {
-        Vel_Atual+=10*aumentar;
-    }
-    public int GetVel_Max()
-    {
-        return Vel_Max;
-    }
-       public int GetVel_Atual()
-    {
-        return Vel_Atual;
+    public int vm{
+        get
+        {
+            return Vel_Max;
+        }
+        set
+        {
+            if(value < 0)
+            {
+                Vel_Max=0;
+            }else if(value > 300)
+            {
+                Vel_Max=300;
+            }else
+            {
+                Vel_Max=value;
+            }
+        }
     }
 }
