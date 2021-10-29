c391b0b34031a962177c1d3e52e534ab166f2258
DavidLucas2021 <dlaredes@gmail.com>
2021-10-28 21:22:48 -0300

    Indexadores de Classes

--- Program.cs
+++ Program.cs
@@ -5,33 +5,33 @@ public class Principal
     static void Main()
     {   
         Carro Uno = new Carro();
-        Uno.vm=340;
-        Console.WriteLine("Velocidade Máxima:...."+Uno.vm);        
+        //Uno[4]=299;
+        Console.WriteLine("Velocidade Máxima:...."+Uno[4]);        
     }
 }
 class Carro
 {
-    private int Vel_Max;
+    private int[] Vel_Max = new int[5]{100,150,230,290,330};
     public Carro()
     {
-        vm=120;
+
     }
-    public int vm{
+    public int this[int i]{
         get
         {
-            return Vel_Max;
+            return Vel_Max[i];
         }
         set
         {
             if(value < 0)
             {
-                Vel_Max=0;
+                Vel_Max[i]=0;
             }else if(value > 300)
             {
-                Vel_Max=300;
+                Vel_Max[i]=300;
             }else
             {
-                Vel_Max=value;
+                Vel_Max[i]=value;
             }
         }
     }
