28b749780c9df332ba3afc6ea81d579faf60d873
DavidLucas2021 <dlaredes@gmail.com>
2021-11-07 10:51:40 -0300

    Métodos que retornam objetos

--- Program.cs
+++ Program.cs
@@ -3,60 +3,42 @@
 public class Principal
 {
     static void Main()
-    {      
-        Carro Uno;
-        Uno.marca="Fiat";
-        Uno.modelo="Uno Way";
-        Uno.cor="Vermelho";
-        Uno.info();
-
-        Carro[] Mobi = new Carro[5];
-        Mobi[0].marca="Fiat";
-        Mobi[0].modelo="Mobi basico";
-        Mobi[0].cor="Lilaz";
-
-        Mobi[1].marca="Fiat";
-        Mobi[1].modelo="Mobi compacto";
-        Mobi[1].cor="azul";
-
-        Mobi[2].marca="Fiat";
-        Mobi[2].modelo="Mobi sedan";
-        Mobi[2].cor="Verde";
-
-        Mobi[3].marca="Fiat";
-        Mobi[3].modelo="Mobi turbo";
-        Mobi[3].cor="Amarelo";
-
-        Mobi[4].marca="Fiat";
-        Mobi[4].modelo="Mobi luxo";
-        Mobi[4].cor="Black";
-
-        //O MÉTODO  PODE SER CHAMADO INFORMANDO A POSIÇÃO DO ARRAAY(UM DE CADA VEZ)
-        
-        // Mobi[0].info();
-        // Mobi[1].info();
-        // Mobi[2].info();
-        // Mobi[3].info();
-        // Mobi[4].info();
-
-        //OU PODE SER USADO O FOR PARA CHAMR O MÉTODO
-
-        for(int i=0;i<Mobi.Length;i++)
-        {
-            Mobi[i].info();
-        }
+    {
+        Galinha Galinha1 = new Galinha("Juricimeia");
+        Galinha Galinha2 = new Galinha("Chicamelia");
+        Galinha Galinha3 = new Galinha("Juriscleideneia");
+        Galinha1.Botar();
+        Galinha1.Botar();
+        Galinha1.Botar();
+        Console.Clear(); 
+        Galinha1.Botar();
+        Galinha2.Botar();
+        Galinha3.Botar();
     }
 }
-struct Carro
+public class Galinha
 {
-    public string marca;
-    public string modelo;
-    public string cor;
-    public void info()
+    private string nome;
+    private int n_ovo;
+    public Galinha(string nome)
     {
-        Console.WriteLine("Marca do carro............."+ this.marca);
-        Console.WriteLine("Marca do carro............."+ this.modelo);
-        Console.WriteLine("Marca do carro............."+ this.cor);
-        Console.WriteLine("____________________________________________");
+        this.nome=nome;
+        n_ovo=0;
+    }
+     public Ovo Botar()
+     {
+        n_ovo++; 
+        return new Ovo(n_ovo,nome);
+     } 
+}
+public class Ovo
+{
+    private int n_ovo;
+    private string nome;
+    public Ovo(int n_ovo,string nome)
+    {
+        this.nome=nome;
+        this.n_ovo=n_ovo;
+        Console.WriteLine("Foi formado " +this.n_ovo +" ovo(s) - " +this.nome);
     }
 }
