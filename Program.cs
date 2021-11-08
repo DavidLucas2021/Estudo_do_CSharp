7d89faf57135e92c2ad916eacf78244dcd3fbb2b
DavidLucas2021 <dlaredes@gmail.com>
2021-11-08 20:07:09 -0300

    Sobrecarga de métodos

--- Program.cs
+++ Program.cs
@@ -4,41 +4,31 @@ public class Principal
 {
     static void Main()
     {
-        Galinha Galinha1 = new Galinha("Juricimeia");
-        Galinha Galinha2 = new Galinha("Chicamelia");
-        Galinha Galinha3 = new Galinha("Juriscleideneia");
-        Galinha1.Botar();
-        Galinha1.Botar();
-        Galinha1.Botar();
-        Console.Clear(); 
-        Galinha1.Botar();
-        Galinha2.Botar();
-        Galinha3.Botar();
+        Calculadora Calculadora = new Calculadora();
+        var resultado=Calculadora.soma(10.5,10);
+        Console.WriteLine(resultado);
     }
 }
-public class Galinha
+class Calculadora
 {
-    private string nome;
-    private int n_ovo;
-    public Galinha(string nome)
+    public int soma(params int[] num)
     {
-        this.nome=nome;
-        n_ovo=0;
+        int res=0;
+
+        for(int i=0;i<num.Length;i++)
+        {
+            res += num[i];
+        }
+        return res;        
     }
-     public Ovo Botar()
-     {
-        n_ovo++; 
-        return new Ovo(n_ovo,nome);
-     } 
-}
-public class Ovo
-{
-    private int n_ovo;
-    private string nome;
-    public Ovo(int n_ovo,string nome)
+     public double soma(params double[] num)
     {
-        this.nome=nome;
-        this.n_ovo=n_ovo;
-        Console.WriteLine("Foi formado " +this.n_ovo +" ovo(s) - " +this.nome);
+        double res=0;
+
+        for(int i=0;i<num.Length;i++)
+        {
+            res += num[i];
+        }   
+        return res;     
     }
 }
