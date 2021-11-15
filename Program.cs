03ea31ecb1661717c5c3890643127b4c7872178c
DavidLucas2021 <dlaredes@gmail.com>
2021-11-15 09:35:54 -0300

    Argumentos de entrada do programa

--- Program.cs
+++ Program.cs
@@ -1,29 +1,20 @@
 ﻿using System;
 
-delegate int Op(int n1, int n2);
 public class Principal
 {
-    static void Main()
+    static void Main(string[] args)
     {
-        int resultado;
-        //Delegate abaixo só precisa ser definido uma vez no código.
-        Op delegate1 = new Op(Calculo.soma); //Pelo motivo da classe não ser static
-        resultado = delegate1(10, 10);         //é necessário fazer a chamada da classe
-        Console.WriteLine("Método soma entrega " + resultado); //"Calculo" para chamar o método.
-
-        delegate1 = new Op(Calculo.mult);
-        resultado = delegate1(10, 15);
-        Console.WriteLine("Método multiplicação entrega " + resultado);
-    }
-}
-public class Calculo
-{
-    public static int soma(int n1, int n2)
-    {
-        return n1 + n2;
-    }
-    public static int mult(int n1, int n2)
-    {
-        return n1 * n2;
+        if(args.Length > 0 )
+        {
+            Console.WriteLine("Quantidade de argumentos: "+args.Length);
+            foreach(string arg in args)
+            {
+                Console.WriteLine("Argumento: " +arg);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Não foram passados argumentos.");
+        }
     }
 }
