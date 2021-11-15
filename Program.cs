5044c729bbc83bc0a0fe4ca4d0f32abe2d1450c3
DavidLucas2021 <dlaredes@gmail.com>
2021-11-15 10:45:01 -0300

    Exceções - Try Catch Finally - P1

--- Program.cs
+++ Program.cs
@@ -2,19 +2,23 @@
 
 public class Principal
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        if(args.Length > 0 )
+        int n1,n2,resultado;
+        resultado=n1=n2=0;
+        n1=10;
+        n2=0;
+
+        try
         {
-            Console.WriteLine("Quantidade de argumentos: "+args.Length);
-            foreach(string arg in args)
-            {
-                Console.WriteLine("Argumento: " +arg);
-            }
+        resultado=n1/n2;//No momento que o bloco Try identifica o erro,ele para
+        //de executar seu trecho e pula para o Catch. Neste caso, ele não imprime a operação
+        Console.WriteLine("{0}/{1}={2}",n1,n2,resultado);
         }
-        else
+        catch(Exception e)//"Exception" é a classe que informa o erro, "e" objeto da classe.
         {
-            Console.WriteLine("Não foram passados argumentos.");
+            Console.WriteLine("Erro: " +e.Message);
+            Console.WriteLine("Exceção: " +e.GetType());
         }
     }
 }
