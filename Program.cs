f52493e846ddd8019eb84322c6aef829a665bb59
DavidLucas2021 <dlaredes@gmail.com>
2021-11-26 20:48:47 -0300

    Coleção Queue(Fila)

--- Program.cs
+++ Program.cs
@@ -5,88 +5,49 @@ using System.Collections.Generic;
     {
         public static void Main()
         {
-            List<string> Carros = new List<string>();
-            List<string> veiculos = new List<string>();
-
-            Carros.Add("uno");
-            Carros.Add("golf");
-            Carros.Add("siena");
-            Carros.Add("punto");
-            //"Insert"=Insere um item na posição especificada.
-            Carros.Insert(4,"capitiva");
-            //"Remove"=REMOVE PELO VALOR DO ELEMENTO 
-            Carros.Remove("uno");
-            //"RemoveAt"=REMOVE PELA POSIÇÃO DO ELEMENTO 
-            Carros.RemoveAt(2);
-            //"Reverse"=INVERTE A POSIÇÃO DOS ITENS DA LISTA
-            Carros.Reverse();
-            //"Sort"=ORDENA OS ELEMENTOS POR ORDEM ALFABÉTICA 
-            Carros.Sort();
+            //string[] nomes= new string[5]{"camaro","civic","cruce","corvette","jetta"};
+            //QUEUE:PRIMEIRO ELEMENTO A ENTRAR, PRIMEIRO ELEMENTO A SAIR 
+            Queue<string> veiculo = new Queue<string>(/*nomes*/);
             
-            //"Count"=PROPRIEDADE QUE RETORNA O 
-            //TAMANHO DA LISTA
-            int tamanho=Carros.Count;
-            Console.WriteLine("Tamnho da lista: " +tamanho);
+            //MÉTODO QUE ADICIONA ELEMENTO NO FINAL DA FILA
+            veiculo.Enqueue("camaro");
+            veiculo.Enqueue("civic");
+            veiculo.Enqueue("cruce");
+            veiculo.Enqueue("corvette");
+            veiculo.Enqueue("jetta");
 
-            //"Capacity"=PROPRIEDADE QUE RETORNA A 
-            //CAPACIDADE DA LISTA
-            int capatidade=20;
-            Carros.Capacity=capatidade;
-            Console.WriteLine("Capacidade da lista: " +capatidade);
+            //"Clear":LIMPA TODO A FILA 
+            //veiculo.Clear();
 
-        
-            //MÉTODO PARA LIMPAR TODA LISTA
-            //Carros.Clear();
+            //"Dequeue":RETORNA O PRIMEIRO ELEMENTO DA FILA E DEPOIS O REMOVE
+            Console.WriteLine("Primeiro elemento da fila:" + veiculo.Dequeue());
 
-            //"Contains"=MÉTODO QUE VERIFICA SE HÁ 
-            //O ELEMENTO ESPECIFICADO NA LISTA
-            if(Carros.Contains("golf"))
+            //REMOVE O PRIMEIRO ELEMENTO DA FILA
+            //veiculo.Dequeue();
+
+            //SOMENTE RETORNA O ELEMENTO DA FILA 
+            Console.WriteLine("Primeiro elemento da fila:" + veiculo.Peek());
+
+            //"Count":RETORNA UM INT COM A QUANTIDADE DE ITENS DA FILA
+            System.Console.WriteLine("Tamanho da Fila: "+veiculo.Count);
+            System.Console.WriteLine("Itens da Fila:");
+            
+            foreach(string car in veiculo)
             {
-                Console.WriteLine("O ELEMENTO ESTÁ NA LISTA");
+                System.Console.WriteLine(car);
+            }
+
+            string c="jetta";
+            //"Contains":MÉTODO QUE VERIFICA SE HÁ O ELEMENTO ESPECIFICADO NA FILA 
+            if(veiculo.Contains(c))
+            {
+                System.Console.WriteLine("O item " +c +" está na fila.");
             }
             else
             {
-                Console.WriteLine("O ELEMENTO NÃO ESTÁ NA LISTA"); 
-            }
-            foreach(string car in Carros)
-            {
-                Console.WriteLine(car);
+                System.Console.WriteLine("O item " +c +" não está na fila.");
             }
 
-            // "AddRange"=MÉTODO QUE É USADO PARA ADICIONAR 
-            // VÁRIOS ELEMENTOS EM UMA LISTA. NO EXEMPLO É
-            // ADICIONADO OS ELEMENTOS DA LISTA "CARROS" EM "VEICULOS" 
-            // veiculos.AddRange(Carros);
-            // foreach(string veiculo in veiculos)
-            // {
-            //    Console.WriteLine(veiculo);
-            // }
-
-            //OUTRA FORMA DE ADICIONAR VARIOS ELEMENTOS 
-            //NA LISTA É USANDO "AddRange" CARREGANDO UM 
-            //ARRAY COM OS ELEMENTOS ABAIXO.
-            // string[] nome_veiculos = new string[]{"celta","cobat","up","jetta","monza"};
-            // veiculos.AddRange(nome_veiculos);
-            // foreach(string veiculo in veiculos)
-            // {
-            //    Console.WriteLine(veiculo);
-            // }
-
-
-            //"CopyTo"=MÉTODO QUE COPIA DA LISTA "CARROS"
-            //PARA O ARRAY "VEICULOS" DEPOIS DA POSIÇÃO 2.
-            //Carros.CopyTo(veiculos,2);
-            // foreach(string veiculo in veiculos)
-            // {
-            //    Console.WriteLine(veiculo);
-            // }
-
-            //"IndexOf"=MÉTODO QUE RETORNA A POSIÇÃO 
-            //DO ELEMENTO ESPECIFICADO.
-            // String c="golf";
-            // int posição=0;
-            // posição=Carros.IndexOf(c);
-            // Console.WriteLine("O CARRO " +c +" ESTA NA POCIÇÃO " +posição);
 
         }
     }
